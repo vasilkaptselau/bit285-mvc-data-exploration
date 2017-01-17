d430f8f00053ddee8bb2689aff92c8b04fe93067
Brian Bansenauer <brianb@cascadia.edu>
2017-01-16 23:38:15 -0800

    added code for Customer View

---
+++ MVC-DataExploration/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_DataExploration.Models;
+
+namespace MVC_DataExploration.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private MyDbContext db = new MyDbContext();
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(db.Customers);
+        }
+    }
+}
---
+++ MVC-DataExploration/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_DataExploration.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public int LoyaltyCardId { get; set; }
+        public int FamilyCount { get; set; }
+    }
+}
